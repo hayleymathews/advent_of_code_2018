85b109940722d04a849c97965bd445a1627a30c2
Hayley Mathews <hayleymathews@Hayley-Mathews.local>
2018-12-04 08:23:03 -0800

    less gross

--- day_01/solution.exs
+++ day_01/solution.exs
@@ -1,6 +1,8 @@
-{:ok, file} = File.open('inputs.csv')
-content = IO.read(file, :all)
-vals = String.split(content, "\n", trim: true) |> Enum.map &String.to_integer/1
-result = Enum.sum(vals)
-IO.puts(result)
-
+case File.read('inputs.csv') do
+  {:ok, content} ->
+    content
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+    |> IO.inspect()
+end
